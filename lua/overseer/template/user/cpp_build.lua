0f4ddaf74643b8bd74bb7c1f6a241667ad7a490c
bwaargh <rawpluganchor@gmail.com>
2024-10-19 17:01:02 -0500

    Starter

---
+++ lua/overseer/template/user/cpp_build.lua
@@ -0,0 +1,16 @@
+return {
+  name = 'Clang C build',
+  builder = function()
+    -- Full path to current file (see :help expand())
+    local file = vim.fn.expand '%:p'
+    local ofile = vim.fn.expand '%:p:r'
+    return {
+      cmd = { 'clang' },
+      args = { '-glldb', '-fstandalone-debug', file, '-o', ofile },
+      components = { { 'on_output_quickfix', open = true }, 'default' },
+    }
+  end,
+  condition = {
+    filetype = { 'c' },
+  },
+}
